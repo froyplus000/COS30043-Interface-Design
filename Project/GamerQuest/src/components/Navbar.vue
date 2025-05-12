1ec268bffd5919b9db180696049aa26e8728efb6
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2025-05-13 09:54:22 +1000

    Update Navbar

--- Project/GamerQuest/src/components/Navbar.vue
+++ Project/GamerQuest/src/components/Navbar.vue
@@ -1,13 +1,42 @@
 <script setup></script>
 
 <template>
-  <nav class="d-flex">
-    <router-link id="logo" class="" to="/">LOGO</router-link>
-
-    <ul>
-      <router-link class="" to="/">Home</router-link>
-      <router-link class="navitem" to="/about">About</router-link>
-      <router-link class="navitem" to="/about"></router-link>
-    </ul>
+  <nav class="bg-brown text-center position-lg-sticky top-0">
+    <div
+      class="container py-1 d-flex flex-column flex-lg-row justify-content-between align-items-center"
+    >
+      <router-link
+        id="logo"
+        class="text-decoration-none text-light fs-1 fw-bold"
+        to="/"
+        >GamerQuest</router-link
+      >
+      <ul
+        id="nav-item"
+        class="list-unstyled fs-4 d-flex flex-column flex-lg-row justify-content-between align-items-center mt-3 gap-lg-3"
+      >
+        <li>
+          <router-link class="navitem" to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link class="navitem" to="/about">About</router-link>
+        </li>
+        <li>
+          <router-link class="navitem" to="/news">News</router-link>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
+
+<style scope>
+.navitem {
+  color: var(--bs-light);
+  text-decoration: none;
+}
+.navitem:hover {
+  color: var(--bs-primary);
+  text-decoration: underline;
+  transition: color 0.3s ease;
+}
+</style>
