620328003cbc7ea8fd37ee4d6fcbb002b7d21265
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2025-05-15 14:14:36 +1000

    Completed and tested on Mercury
    - Successfully deployed to Mercury
    - MariaDB is working

--- Project/GamerQuest/backend/db.php
+++ Project/GamerQuest/backend/db.php
@@ -6,14 +6,14 @@ if (session_status() === PHP_SESSION_NONE) {
 require_once '../lib/password.php';
 
 
-$db_server = 'feenix-mariadb.swin.edu.au';
-$db_user = 's103883220';
-$db_pw = 'FolkPt240641@';
-$db_name = 's103883220_db';
-// $db_server = 'localhost';
-// $db_user = 'root';
-// $db_pw = 'root';
-// $db_name = 'GamerQuest';
+// $db_server = 'feenix-mariadb.swin.edu.au';
+// $db_user = 's103883220';
+// $db_pw = 'FolkPt240641@';
+// $db_name = 's103883220_db';
+$db_server = 'localhost';
+$db_user = 'root';
+$db_pw = 'root';
+$db_name = 'GamerQuest';
 
 $conn = mysqli_connect($db_server, $db_user, $db_pw, $db_name);
 mysqli_set_charset($conn, 'utf8');
